cd8393eb37acc33bd82f77b006d3a8135aced09c
Juliana Ifionu <julieifionu@gmail.com>
2021-09-23 08:35:41 +0100

    Solution

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,3 +1,6 @@
 module.exports = function (str) {
   // write your code here
+  const newString = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  const reversedStr = newString.split('').reverse().join('');
+  return newString === reversedStr;
 }
